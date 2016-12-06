a21107a700848ce86e0608796e55b42d96afcacb
Tobias Tekampe <ttekampe@cern.ch>
2016-12-06 10:18:05 +0100

    Functions to merge DiracFiles directly

---
+++ merge.py
@@ -0,0 +1,52 @@
+import os
+import subprocess
+
+
+def create_lfn_list(j):
+    '''
+    Takes a job as input and returns a list containig the lfns of all
+    DiracFiles the job created.
+    '''
+    lfns = []
+    for sj in j.subjobs:
+        for df in sj.outputfiles.get(DiracFile):
+            lfns.append(df.lfn)
+    return lfns
+
+
+def create_txt_for_hadd(j, name):
+    '''
+    Takes a job and a name as input.
+    Creates a txt file called name and adds all DiracFiles the job craeted
+    to the file, such that it can be passed to hadd
+    '''
+    if not name.endswith('.txt'):
+        if '.' in name:
+            print('Name needs to end in .txt')
+            return
+        name = name + '.txt'
+
+    lfns = create_lfn_list(j)
+    gangascriptdir = os.environ['HOME'] + "/gangascripts"
+    lfn_urls = subprocess.check_output(
+        [
+            "lb-run",
+            "LHCbDirac",
+            "prod",
+            "python",
+            gangascriptdir + "/get_access_urls.py"
+        ] + lfns
+    ).split('\n')
+
+    for line in lfn_urls[:]:
+        if 'SRM_FILE_UNAVAILABLE' in line:
+            print(line)
+        if not line.startswith('root://'):
+            lfn_urls.remove(line)
+
+    with open(name, 'w') as f:
+        for lfn in lfn_urls:
+            f.write('"' + lfn + '"\n')
+    print('Done. Now run')
+    print('hadd -ff <taget> @{}'.format(name))
+    print('Make sure to have a valid grid token at all time')
